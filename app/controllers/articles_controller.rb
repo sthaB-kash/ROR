bd0350ae722a83e91fe68b9622610ecdbf17f2e0
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-01-26 21:40:58 +0545

    feat(comment): add model comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path, status: :see_other 
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
