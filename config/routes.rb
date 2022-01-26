416fc3b58dcc04771e42731bfac1e9e50b0e2ae2
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-01-26 13:50:28 +0545

    feat(articles): display article by id

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   root "articles#index"
   get "/about-us", to: "about#index", as: :about #get "/about", to: "about#index" 
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
